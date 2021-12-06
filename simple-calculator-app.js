279e73d10dbd89b6e2d441c872c9fb9a09ae4831
Divine-Favour Oladejo <fdivine0502@gmail.com>
2021-12-06 22:02:26 +0100

    innit

---
+++ simple-calculator-app.js
@@ -0,0 +1,133 @@
+const calculator = {
+    displayValue: '0',
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: null,
+  };
+  
+  function updateDisplay() {
+    const display = document.querySelector('.calculator-screen');
+    display.value = calculator.displayValue;
+  }
+  
+  updateDisplay();
+  
+  const keys = document.querySelector('.calculator-keys');
+  keys.addEventListener('click', event => {
+    const { target } = event;
+    const { value } = target;
+    if (!target.matches('button')) {
+      return;
+    }
+  
+    switch (value) {
+      case '+':
+      case '-':
+      case '*':
+      case '/':
+      case '=':
+        handleOperator(value);
+        break;
+      case '.':
+        inputDecimal(value);
+        break;
+      case 'delete' :
+          remove();
+        break;
+      case 'all-clear':
+        resetCalculator();
+        break;
+      default:
+        // check if the key is an integer
+        if (Number.isInteger(parseFloat(value))) {
+          inputDigit(value);
+        }
+    }
+  
+    updateDisplay();
+  });
+  
+  function inputDigit(digit) {
+    const { displayValue, waitingForSecondOperand } = calculator;
+  
+    if (waitingForSecondOperand === true) {
+      calculator.displayValue = digit;
+      calculator.waitingForSecondOperand = false;
+    } else {
+      calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+    }
+  
+    console.log(calculator);
+  }
+  
+  
+  
+  
+  function inputDecimal(dot) {
+    // If the `displayValue` property does not contain a decimal point
+    
+    if (calculator.waitingForSecondOperand === true) {
+        calculator.displayValue = '0.'
+      calculator.waitingForSecondOperand = false;
+      return
+    }
+  
+    if (!calculator.displayValue.includes(dot)) {
+      // Append the decimal point
+      calculator.displayValue += dot;
+    }
+  }
+  
+  function handleOperator(nextOperator) {
+    // Destructure the properties on the calculator object
+    const { firstOperand, displayValue, operator } = calculator
+    // `parseFloat` converts the string contents of `displayValue`
+    // to a floating-point number
+    const inputValue = parseFloat(displayValue);
+    
+     if (operator && calculator.waitingForSecondOperand)  {
+      calculator.operator = nextOperator;
+      console.log(calculator);
+      return;
+    }
+  
+    // verify that `firstOperand` is null and that the `inputValue`
+    // is not a `NaN` value
+    if (firstOperand === null && !isNaN(inputValue)) {
+      // Update the firstOperand property
+      calculator.firstOperand = inputValue;
+    } else if (operator) {
+      const result = calculate(firstOperand, inputValue, operator);
+  
+      calculator.displayValue = `${parseFloat(result.toFixed(7))}`;
+      calculator.firstOperand = result;
+    }
+  
+    calculator.waitingForSecondOperand = true;
+    calculator.operator = nextOperator;
+  }
+  
+  console.log(calculator);
+  
+  function calculate(firstOperand, secondOperand, operator) {
+    if (operator === '+') {
+      return firstOperand + secondOperand;
+    } else if (operator === '-') {
+      return firstOperand - secondOperand;
+    } else if (operator === '*') {
+      return firstOperand * secondOperand;
+    } else if (operator === '/') {
+      return firstOperand / secondOperand;
+    } 
+    
+  
+    return secondOperand;
+  }
+  
+  function resetCalculator() {
+    calculator.displayValue = '0';
+    calculator.firstOperand = null;
+    calculator.waitingForSecondOperand = false;
+    calculator.operator = null;
+    console.log(calculator);
+  }
